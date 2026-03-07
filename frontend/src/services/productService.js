407d97a1ff5e4ee67f13f321b684fae19624edb6
saharat151246 <saharat151246@gmail.com>
2026-03-07 21:58:45 +0700

    Update API URL to use environment variable

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = "http://localhost:5000/api/products"
+const API = `${import.meta.env.VITE_API_URL || 'http://localhost:5000'}/api/products`
 
 export const getProducts = () => axios.get(API)
 
